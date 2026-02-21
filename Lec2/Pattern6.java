9eea2b7d8bea336f9399998ebdc9e9fbb4f8db5b
monukumar98 <monu012as@gmail.com>
2026-02-21 12:50:25 +0530

    Pattern

---
+++ Lec2/Pattern6.java
@@ -0,0 +1,31 @@
+package Lec2;
+
+public class Pattern6 {
+	public static void main(String[] args) {
+		int n = 5;
+		int row = 1;
+		int star = n;
+		int space = 0;
+		while (row <= n) {
+			// space
+			int i = 1;
+			while (i <= space) {
+				System.out.print("  ");
+				i++;
+			}
+
+			// star
+			int j = 1;
+			while (j <= star) {
+				System.out.print("* ");
+				j++;
+			}
+			// next line ki Prep
+			System.out.println();
+			row++;
+			star--;
+			space += 2;// space = space +2 ;
+		}
+
+	}
+}
