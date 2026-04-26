cb126b84a7a4209badacc7a7e69c0ef907ed0d9e
monukumar98 <monu012as@gmail.com>
2026-04-26 12:09:08 +0530

    Recursion backtking

---
+++ Lec18/QueenCombintation.java
@@ -0,0 +1,29 @@
+package Lec18;
+
+public class QueenCombintation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 4;
+		int tq = 2;
+		boolean[] board = new boolean[n];
+		Combintation(board, tq, "", 0, 0);
+
+	}
+
+	public static void Combintation(boolean[] board, int tq, String ans, int qpsf, int idx) {
+		if (qpsf == tq) {
+			System.out.println(ans);
+			return;
+		}
+		for (int i = idx; i < board.length; i++) {
+			if (board[i] == false) {
+				board[i] = true;
+				Combintation(board, tq, ans + "b" + i + "q" + qpsf, qpsf + 1, i + 1);
+				board[i] = false;
+			}
+		}
+
+	}
+
+}
