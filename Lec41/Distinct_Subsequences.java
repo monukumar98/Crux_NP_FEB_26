221567fdf31665d8925b4e9029cb3583976909df
monukumar98 <monu012as@gmail.com>
2026-07-26 12:12:44 +0530

    DP-2

---
+++ Lec41/Distinct_Subsequences.java
@@ -0,0 +1,26 @@
+package Lec41;
+
+public class Distinct_Subsequences {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "rabbbit", t = "rabbit";
+	}
+
+	// i--> coin s | j--> amount t
+	public static int Coin_Change(String s, String t, int i, int j) {
+		if (j == t.length()) {
+			return 1;
+		}
+		if (i == s.length()) {
+			return 0;
+		}
+		int inc = 0, exc = 0;
+		if (s.charAt(i) == t.charAt(j)) {
+			inc = Coin_Change(s, t, i + 1, j + 1);
+		}
+		exc = Coin_Change(s, t, i + 1, j);
+		return inc + exc;
+	}
+
+}
---
+++ Lec41/Uncrossed_Lines.java
@@ -0,0 +1,27 @@
+package Lec41;
+
+public class Uncrossed_Lines {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] nums1 = {1,4,2}, nums2 = {1,2,4};
+	}
+	public static int LCSBU(int[] text1, int [] text2) {
+		int[][] dp = new int[text1.length + 1][text2.length + 1];
+		for (int i = 1; i < dp.length; i++) {
+			for (int j = 1; j < dp[0].length; j++) {
+				if (text1[i - 1] == text2[j - 1]) {
+					dp[i][j] = 1 + dp[i - 1][j - 1];
+				} else {
+					int f = dp[i - 1][j];
+					int s = dp[i][j - 1];
+					dp[i][j] = Math.max(f, s);
+				}
+
+			}
+		}
+		return dp[dp.length - 1][dp[0].length - 1];
+	}
+
+
+}
